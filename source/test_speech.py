5e5db538378420b06e7bd8e0d800a303cd4bb828
ReznikovRoman <romanreznikov2002@yandex.ru>
2020-06-29 22:06:58 +0300

    Default msg and delta

--- source/test_speech.py
+++ source/test_speech.py
@@ -25,24 +25,38 @@ def say_msg(msg):
     tts.runAndWait()  # Воспроизвести очередь реплик и дождаться окончания речи
 
 
-first_delta = 5
-second_delta = 20
-
-
-def create_msg():
-    msg = "Пора сделать перерыв"
+def create_msg(msg):
     say_msg(msg)
     notification.notify(
         title='Перерыв',
-        message='Пора сделать перерыв',
+        message=msg,
         app_name='SpeechNotification',
     )
 
 
-while True:
-    create_msg()
-    time.sleep(first_delta)
-    create_msg()
-    time.sleep(second_delta)
+def main():
+    first_delta = 5
+
+    # Inputs
+    # is_change = int(input("Do you want to change settings - yes-1, no-2 ?: "))
+    # is_change = True if is_change == 1 else False
+
+    # if is_change:
+    #     custom_msg = input("Type in your custom message: ")
+    ''''''
+    # TODO: Добавать изменение настроек + профили
+
+    default_delta = 2700  # 45 min
+    default_msg = "Пора сделать перерыв"
+
+    while True:
+        create_msg(default_msg)
+        time.sleep(first_delta)
+        create_msg(default_msg)
+        time.sleep(default_delta)
+
+
+if __name__ == '__main__':
+    main()
 
 
